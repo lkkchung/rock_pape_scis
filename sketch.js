cb9a5e5f3a1ec82d6066003c8c69e2515cc8b2fb
Lucas <lucas@Macbizzle.home>
2018-10-15 23:14:13 -0400

    Beginning OSC integration

--- sketch.js
+++ sketch.js
@@ -10,14 +10,14 @@ let reset = true;
 let screenTimer = 0;
 let interval = 0;
 let result = 0;
-let resultMessage = ["YOU WIN!", "YOU LOSE.", "DRAW..."];
+let resultMessage = ["you win!", "you lose.", "draw..."];
 let round = 0;
 let confidence = 0;
 
 let brain;
 let trainingRounds = 20;
 let history = [];
-let hLength = 1 * 3;
+let hLength = 10 * 3;
 
 let paperPNG;
 let rockPNG;
@@ -34,13 +34,15 @@ function preload() {
   rockPNG = loadImage('rock.png');
   scissorsPNG = loadImage('scissors.png');
   welcomePNG = loadImage('welcome.png');
-  titlePNG = loadImage('RPS_Title.png');  
+  titlePNG = loadImage('RPS_Title.png');
 }
 
 function setup() {
   // createCanvas(browserSize.browserWidth, browserSize.browserHeight);
   createCanvas(800, 800);
   brain = new NeuralNetwork(hLength, 60, 3);
+  textFont('courier new');
+  setupOsc(3333, 3334);
 
 }
 
@@ -72,7 +74,7 @@ function draw() {
     }
     countdownScreen(interval);
 
-    
+
     // pChoose();
   }
 
@@ -132,38 +134,54 @@ function draw() {
   if (screenState >= 2) {
     textAlign(CENTER);
     textSize(50);
+    textStyle(BOLD);
     fill(255);
-    rectMode(CENTER);
-    rect(width / 2, 80, 200, 120);
-    fill(0);
-    text("ROUND", width / 2, 70);
+    // rectMode(CENTER);
+    // rect(width / 2, 80, 200, 120);
+    // fill(0);
+    text("round", width / 2, 70);
     text(round, width / 2, 130);
 
     fill(255);
     textSize(20);
     textAlign(CENTER);
-    text("Computer", width / 4, 40);
-    text("Player", width * 3 / 4, 40);
+    text("computer", width / 4, 70);
+    text("player", width * 3 / 4, 70);
     textSize(30);
-    text(npScore, width / 4, 90);
-    text(pScore, width * 3 / 4, 90);
+    text(npScore, width / 4, 130);
+    text(pScore, width * 3 / 4, 130);
   }
 }
 
 function introScreen() {
   push();
-  translate(width / 2, 300);
+  translate(width / 2, 0);
+  // textSize(20);
+  // fill(255);
+  // text("Welcome to", 0, 0);
+  // textSize(60);
+  // textStyle(BOLD);
+
+  push();
+  translate(0, 180);
+  imageMode(CENTER);
+  scale(1 / 4);
+  image(titlePNG, 0, 0);
+  pop();
+
+  // text("Rock, Paper, Scissors", 0, 65);
+  textSize(20);
+  fill(255, 0, 60);
   textAlign(CENTER);
-  textSize(20);
-  fill(255);
-  text("Welcome to", 0, 0);
-  textSize(60);
   textStyle(BOLD);
-  text("Rock, Paper, Scissors", 0, 65);
-  textSize(20);
-  fill(255, 0, 0);
-  textStyle(ITALIC);
-  text("Machine Learning Edition", 0, 110);
+  text("machine learning edition", 0, 230);
+
+  push();
+  translate(-20, 480);
+  imageMode(CENTER);
+  scale(1 / 4);
+  image(welcomePNG, 0, 0);
+  pop();
   pop();
 
   // screenTimer -= 1;
@@ -178,24 +196,24 @@ function instructScreen() {
   push();
   translate(width / 2, 300);
   textAlign(CENTER);
-  textSize(60);
+  textSize(40);
   textStyle(BOLD);
   fill(255);
-  text("How to play:", 0, 0);
+  text("how to play:", 0, 0);
   textSize(20);
   textStyle(NORMAL);
-  text("Each player simultaneously plays an item, either rock, paper, or scissors.", 0, 40);
-  text("Rock(1) beats scissors.", 0, 70);
-  text("Scissors(2) beats paper.", 0, 100);
-  text("Paper(3) beats rock.", 0, 130);
+  text("each player simultaneously plays an item," + '\n' + "either rock, paper, or scissors.", 0, 40);
+  text("rock(1) beats scissors.", 0, 100);
+  text("scissors(2) beats paper.", 0, 130);
+  text("paper(3) beats rock.", 0, 160);
   pop();
 
   if (screenTimer <= 120) {
     textSize(60);
     textAlign(CENTER);
     textStyle(BOLD);
-    fill(255, 0, 0);
-    text("READY?!", width / 2, height / 2);
+    fill(255, 0, 60);
+    text("ready?!", width / 2, 600);
     screenTimer += 1;
   }
 
@@ -213,15 +231,19 @@ function countdownScreen(_i) {
   textSize(100);
   textStyle(BOLD)
   textAlign(CENTER);
-  fill(255, 0, 0);
+  fill(255, 0, 60);
   if (screenTimer > 120) {
+    // textSize(240 - screenTimer);
     text("3", 0, 0);
   } else if (screenTimer > 80) {
+    // textSize(200 - screenTimer);
     text("2", 0, 0);
   } else if (screenTimer > 40) {
+    // textSize(160 - screenTimer);
     text("1", 0, 0);
   } else {
-    text("THROW!", 0, 0);
+    textSize(120 - screenTimer);
+    text("throw!", 0, 0);
   }
   pop();
 
@@ -243,27 +265,63 @@ function throwScreen() {
 
   textSize(50);
   textStyle(NORMAL);
-  fill(255, 255, 0);
-  text(rps[npChoice], width / 4, height / 2);
-  text(rps[pChoice], width * 3 / 4, height / 2);
+  fill(255, 203, 0);
 
-  if(confidence > 0){
+  push();
+  translate(width / 4, width / 2 - 80);
+  imageMode(CENTER);
+  scale(-1 / 3, 1 / 3);
+  rotate(-PI / 2);
+  if(npChoice == 1){
+    image(rockPNG, 0, 0);
+  }
+  if(npChoice == 2){
+    image(paperPNG, 0, 0);
+  }
+  if(npChoice == 3){
+    image(scissorsPNG, 0, 0);
+  }
+  pop();
+
+  push();
+  translate(3 * width / 4, width / 2 - 80);
+  imageMode(CENTER);
+  scale(1 / 3, 1 / 3);
+  rotate(-PI / 2);
+  if(pChoice == 1){
+    image(rockPNG, 0, 0);
+  }
+  if(pChoice == 2){
+    image(paperPNG, 0, 0);
+  }
+  if(pChoice == 3){
+    image(scissorsPNG, 0, 0);
+  }
+  pop();
+
+
+  // text(rps[npChoice], width / 4, height / 2);
+  // text(rps[pChoice], width * 3 / 4, height / 2);
+
+  if (confidence > 0) {
     textSize(20);
     text(Math.round(confidence * 100) + "% confident", width / 4, height / 2 + 80);
   }
 
   if (screenTimer <= 100) {
-    rectMode(CENTER);
-    fill(80);
-    if (result == 0) {
-      fill(0, 255, 0);
-    }
-    if (result == 1) {
-      fill(255, 0, 0);
-    }
-    rect(width / 2, height - 180, 600, 125)
-    textSize(100);
+    // rectMode(CENTER);
+    // fill(80);
+    // if (result == 0) {
+    //   fill(0, 255, 0);
+    // }
     fill(255);
+    if (result == 1) {
+      fill(255, 0, 60);
+    }
+    // rect(width / 2, height - 180, 600, 125)
+    textSize(60);
+    // fill(255, 0, 60);
+    textStyle(BOLD);
     text(resultMessage[result], width / 2, height - 150);
   }
 
@@ -272,8 +330,8 @@ function throwScreen() {
   if (screenTimer <= 0) {
     screenState = 2;
     reset = true;
-    
-    if (round >= 10){
+
+    if (round >= 10) {
       training();
     }
 
@@ -341,8 +399,8 @@ function npChoose() {
     history.push(1);
   }
 
-  if (history.length > 30){
-    history.splice(0,3);
+  if (history.length > hLength) {
+    history.splice(0, 3);
   }
 }
 
@@ -372,157 +430,36 @@ function training() {
   let targets;
 
   //if the player misses a throw, any response will be fine
-  if (pChoice == 0 || pChoice == 1){
-    targets = [1,0,0];
+  if (pChoice == 0 || pChoice == 1) {
+    targets = [1, 0, 0];
   }
-  if (pChoice == 2){
-    targets = [0,1,0];
+  if (pChoice == 2) {
+    targets = [0, 1, 0];
   }
-  if (pChoice == 3){
-    targets = [0,0,1];
+  if (pChoice == 3) {
+    targets = [0, 0, 1];
   }
-    
+
   let inputs = history;
 
   brain.train(inputs, targets);
 }
 
-
-
-
-
-
-
-// let bases = ["kaiser roll", "brioche bun", "wonder bread", "cookie",
-//   "focaccia", "hero", "whole wheat bread"
-// ];
-
-// let ings1 = ["ham", "roast beef", "peanut butter", "bologna", "chocolate ice cream",
-//   "hamburger", "veggie burger"
-// ];
-// let ings2 = ["cheddar", "swiss cheese", "strawberry jelly", "mayonnaise",
-//   "vanilla ice cream", "arugula", "ketchup"
-// ];
-
-// let yum = ["delicious!", "yummy!", "yes, please!", "tasty!"];
-// let gross = ["gross!", "no, thank you", "eww.", "disgusting.", "barf."];
-
-// let delicious;
-// let round = 0;
-// let indices = [0, 1, 2, 3, 4, 5, 6];
-// let bLike;
-// let bDislike;
-// let sandwichGo = true;
-// let sand;
-// let continuing = false;
-// let trainingRounds = 10;
-
-// function setup() {
-//   brain = new NeuralNetwork(3, 6, 2);
-
-//   bLike = createButton(random(yum));
-//   bLike.position(20, 150);
-//   bLike.mousePressed(isDelicious);
-
-//   bDislike = createButton(random(gross));
-//   bDislike.position(150, 150);
-//   bDislike.mousePressed(notDelicious);
-
-// }
-
-// function draw() {
-//   if (sandwichGo == true) {
-//     sand = makeSandwich();
-
-//     bLike.html(random(yum));
-//     bDislike.html(random(gross));
-//   }
-
-//   let sB = sand[0];
-//   let sI1 = sand[1];
-//   let sI2 = sand[2];
-
-//   createCanvas(400, 200);
-//   background(240);
-
-//   fill(0);
-
-
-//   if (round < trainingRounds) {
-
-//     let tCount = round + 1;
-//     text("Does this sandwich sound delicious?" + " (" + tCount + "/10)", 20, 20);
-
-//   } else {
-//     let sandRec
-//     if (continuing == true) {
-//       sandRec = sandwichPredictor();
-//     }
-//     if (sandRec == true) {
-//       text("I recommend this sandwich for you", 20, 20);
-//     } else {
-//       text("I don't recommend this sandwich for you", 20, 20);
-//     }
-//   }
-
-//   text(bases[sB], 20, 40);
-//   text(ings1[sI1], 20, 60);
-//   text(ings2[sI2], 20, 80);
-//   text(bases[sB], 20, 100);
-
-// }
-
-// function makeSandwich() {
-//   let sB = random(indices);
-//   let sI1 = random(indices);
-//   let sI2 = random(indices);
-
-//   sandwichGo = false;
-
-//   return [sB, sI1, sI2];
-// }
-
-// function sandwichPrefs(_t) {
-//   let targets;
-//   if (_t == true) {
-//     targets = [0, 1];
-//   } else {
-//     targets = [1, 0];
-//   }
-
-//   let sB = sand[0];
-//   let sI1 = sand[1];
-//   let sI2 = sand[2];
-
-//   let inputs = [sB / bases.length, sI1 / ings1.length, sI2 / ings2.length];
-
-//   brain.train(inputs, targets);
-// }
-
-// function sandwichPredictor() {
-//   let trySand = brain.predict(sand);
-//   console.log(trySand);
-//   continuing = false;
-
-//   if (trySand[1] > trySand[0]) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// function isDelicious() {
-//   delicious = true;
-//   sandwichGo = true;
-//   continuing = true;
-//   sandwichPrefs(delicious);
-//   round++;
-// }
-
-// function notDelicious() {
-//   delicious = false;
-//   sandwichGo = true;
-//   continuing = true;
-//   sandwichPrefs(delicious);
-//   round++;
-// }
+function setupOsc(oscPortIn, oscPortOut) {
+	var socket = io.connect('http://127.0.0.1:8081', { port: 8081, rememberTransport: false });
+	socket.on('connect', function() {
+		socket.emit('config', {	
+			server: { port: oscPortIn,  host: '127.0.0.1'},
+			client: { port: oscPortOut, host: '127.0.0.1'}
+		});
+	});
+	socket.on('message', function(msg) {
+		if (msg[0] == '#bundle') {
+			for (var i=2; i<msg.length; i++) {
+				receiveOsc(msg[i][0], msg[i].splice(1));
+			}
+		} else {
+			receiveOsc(msg[0], msg.splice(1));
+		}
+  });
+}
